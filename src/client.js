22715755058fb7becac28c84025458e7c03b0ed2
Yassine Khaidouch <yassineuav@gmail.com>
2022-07-15 19:39:51 -0600

    add sanity client ignore btw

--- src/client.js
+++ src/client.js
@@ -2,11 +2,12 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: `${process.env.REACT_APP_SANITY_PROJECT_ID}`,
   dataset: 'production',
   apiVersion: '2022-07-04',
   useCdn: true,
+  projectId: `${process.env.REACT_APP_SANITY_PROJECT_ID}`,
   token: `${process.env.REACT_APP_SANITY_TOKEN}`,
+  ignoreBrowserTokenWarning: true,
 });
 
 const builder = imageUrlBuilder(client);
